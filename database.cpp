871930abb9a645a80acc505b9125bbe8f34c8980
Ana F <whoisfomina@gmail.com>
2023-10-16 21:17:26 +0300

    optimization in del and find by switching to map<Date, vector<str>>

--- database.cpp
+++ database.cpp
@@ -2,49 +2,61 @@
 
 void Database::Add(const Date& date, const std::string& event){
 	std::string temp = date.PrintDate() + event;
-	if (dates_.count(temp)) return;
-	dates_.insert(temp);
-	db_.insert(std::pair<Date, std::string>(date, event));
+	if (dates_m.count(temp)) return; //delete?
+	dates_m.insert(temp);
+	db_m[date].push_back(event);
 }
 
 void Database::Print(std::ostream& os) const {
-	for (const auto& elem: db_) { //перенести преобразования сюда?
-		os << elem.first << " " << elem.second << std::endl;
+	for (const auto& elem: db_m) { 
+		for (auto it = elem.second.begin(); it != elem.second.end(); ++it) //the order is mangled!
+		os << elem.first << " " << *it << std::endl;
 	}
 }
 
 std::string Database::Last(const Date& date) const {
-	auto last_date = std::upper_bound(db_.begin(), db_.end(), date, 
-			[](const Date& val, const std::pair<Date, std::string>& date_in_db) {
+	auto last_date = std::upper_bound(db_m.begin(), db_m.end(), date, 
+			[](const Date& val, const db_elem_t& date_in_db) {
 			return  date_in_db.first > val;
 			});
-	if (last_date == db_.begin()) {
+	if (last_date == db_m.begin()) {
 		return "No entries";
 	}
 	auto target = std::prev(last_date);
-	std::string res = (*target).first.PrintDate() + " " + (*target).second;
+	size_t last_event = (*target).second.size() - 1;
+	std::string res = (*target).first.PrintDate() + " " + (*target).second[last_event];
 	return res;
 } 
 
 int Database::RemoveIf(std::function<bool(const Date&, const std::string&)> predicat) {
 	int count = 0;
-	for (auto it = db_.begin(); it != db_.end();) {
+	for (auto it = db_m.begin(); it != db_m.end();) {
 		auto this_it = it++;
 		auto element = *this_it;
-		if (predicat(element.first, element.second)) {
-			db_.erase(this_it);
-			dates_.erase(element.first.PrintDate()+element.second);
-			++count;
+		if (predicat(element.first, "")) db_m.erase(element.first); //if it refers to the date, del the whole entry
+		else { 
+			auto res = std::stable_partition(element.second.begin(), element.second.end(), [=] (const std::string& str) {
+				return !predicat(element.first, str); //evals to true for the ones to delete
+			});
+			if (res == element.second.end()) continue;
+			if (res == element.second.begin()) db_m.erase(element.first);
+			else {
+				auto last_elem = std::prev(element.second.end());
+				while(last_elem <= res) {
+					auto this_it = last_elem--;
+					element.second.erase(this_it);
+				}
+			}
 		}
 	}
 	return count;
 }
 
-
+//fix for the map solution
 std::vector<std::string> Database::FindIf(std::function<bool(const Date&, const std::string&)> predicat) const {
 	int count = 0;
 	std::vector<std::string> res = {};
-	for (auto it = db_.begin(); it != db_.end();) {
+	for (auto it = db_m.begin(); it != db_m.end();) {
 		auto element = *it;
 		if (predicat(element.first, element.second)) {
 			res.push_back(element.first.PrintDate() + " " + element.second);
--- database.h
+++ database.h
@@ -9,6 +9,9 @@
 
 class Database {
     public:
+    using db_t = std::map<Date, std::vector<std::string>>;
+    using db_elem_t = std::pair<Date, std::vector<std::string>>;
+    
     Database() {}
     void Add(const Date& date, const std::string& event);
     void Print(std::ostream& os) const;
@@ -17,6 +20,7 @@ class Database {
     std::string Last(const Date& date) const;
 
     private:
-    std::multimap<Date, std::string> db_;
-    std::set<std::string> dates_;
+    //std::multimap<Date, std::string> db_; 
+    db_t db_m;
+    std::set<std::string> dates_m;
 };
